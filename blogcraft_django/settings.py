cf8ac95ffa18d12198a97a8d89cfe68b6d5547c3
SuaJeong-winter <sua.jeong.developer@gmail.com>
2025-07-02 15:52:09 +0900

    feat: add image uploadd functionality to post_list page

--- blogcraft_django/settings.py
+++ blogcraft_django/settings.py
@@ -126,6 +126,10 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+# Image Upload
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
