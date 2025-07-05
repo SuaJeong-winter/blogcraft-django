37c446c6e3207e73b6d6abd0764a950b84a981e9
SuaJeong-winter <sua.jeong.developer@gmail.com>
2025-07-05 11:59:21 +0900

    feat: add author field

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
-    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
-    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'  # 포스트 목록에 작성자 정보 출력
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
