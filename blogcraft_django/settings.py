7374431ebe0369a2a591405429ba12997ccce410
SuaJeong-winter <sua.jeong.developer@gmail.com>
2025-07-08 11:57:58 +0900

    style: refactor form UI pages using django-crispy-forms

--- blogcraft_django/settings.py
+++ blogcraft_django/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
 
+    'crispy_forms',
+    'crispy_bootstrap4',  # 추가
+
     'blog',
     'single_pages',
 ]
@@ -125,6 +128,8 @@ STATIC_URL = 'static/'
 # Image Upload
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
